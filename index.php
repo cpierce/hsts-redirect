3404106c4dbaab033f808506d145f4111e19eee1
Chris Pierce <cpierce@csdurant.com>
2023-12-21 09:27:02 -0600

    refactor and a new phpstan

--- index.php
+++ index.php
@@ -1,33 +1,28 @@
 <?php
+// Define redirection rules
+$redirections = [
+    'docs' => 'https://drive.google.com/a/',
+    'drive' => 'https://drive.google.com/a/',
+    'calendar' => 'https://www.google.com/calendar/hosted/',
+    'sites' => 'https://sites.google.com/a/',
+    'groups' => 'https://groups.google.com/a/',
+    'default' => 'https://mail.google.com/a/' // Default redirection
+];
+
 $domain = $_SERVER['HTTP_HOST'];
 $dot_split = explode('.', $domain);
 $subdomain = $dot_split[0];
+
+// Initialize the googleWorkspaceDomain variable
+$googleWorkspaceDomain = '';
+
 foreach ($dot_split as $key => $value) {
     if ($key > 0) {
         $googleWorkspaceDomain .= '.' . $value;
     }
 }
 $googleWorkspaceDomain = ltrim($googleWorkspaceDomain, '.');
-switch ($subdomain) {
-    case 'docs':
-    case 'drive':
-        // Redirect to the google drive site.
-        header('Location: https://drive.google.com/a/' . $googleWorkspaceDomain, true, 301);
-        break;
-    case 'calendar':
-        // Redirect to the google calendar site.
-        header('Location: https://www.google.com/calendar/hosted/' . $googleWorkspaceDomain, true, 301);
-        break;
-    case 'sites':
-        // Redirect to the google sites site.
-        header('Location: https://sites.google.com/a/' . $googleWorkspaceDomain, true, 301);
-        break;
-    case 'groups':
-        // Redirect to the google groups site.
-        header('Location: https://groups.google.com/a/' . $googleWorkspaceDomain, true, 301);
-        break;
-    default:
-        // Redirect to the main gmail site.
-        header('Location: https://mail.google.com/a/' . $googleWorkspaceDomain, true, 301);
-        break;
-}
+
+// Redirect based on the subdomain
+$redirectURL = $redirections[$subdomain] ?? $redirections['default'];
+header('Location: ' . $redirectURL . $googleWorkspaceDomain, true, 301);
